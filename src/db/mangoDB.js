087c857d435cb3157c47f47591ceeb412fcade61
khanakitclass <khanakitclass@gmail.com>
2026-07-20 08:03:12 +0530

    sd

--- src/db/mangoDB.js
+++ src/db/mangoDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectMangoDB = async () => {
     try {
-        const connection = await mongoose.connect('mongodb://127.0.0.1:27017/newdata');
+        const connection = await mongoose.connect(process.env.MONGODB_URI);
 
         console.log("Connected to MongoDB", connection.connection.host);
     } catch (error) {
